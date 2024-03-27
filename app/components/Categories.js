f5316dde4f77be78cf44c93cced5dfcfe5ad4b22
Shakil57375 <shakil57375@gmail.com>
2024-03-28 00:12:15 +0600

    added

--- app/components/Categories.js
+++ app/components/Categories.js
@@ -11,9 +11,12 @@ const Categories = ({ category }) => {
             <main>
                 <section class="w-11/12 lg:w-10/12 max-w-7xl  mx-auto py-0 lg:py-10 lg:flex justify-between items-start">
                     <div class="w-full flex items-center justify-between lg:block lg:w-2/12 my-10 lg:my-0 lg:mt-4">
-                        <button class="hover:border-b border-black block h-6 box-border mt-4">
+                        <Link
+                            href={`/category/fragrances`}
+                            class="hover:border-b border-black block h-6 box-border mt-4"
+                        >
                             All
-                        </button>
+                        </Link>
                         <Link
                             href={`/category/smartphones`}
                             class="hover:border-b border-black block h-6 box-border mt-5"
